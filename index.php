45f8b8a7871ceeed465ebca37fc98361bf12ab75
MuchamadEvanHidayat <104417269+MuchamadEvanHidayat@users.noreply.github.com>
2022-04-26 14:03:15 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- Google Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@400;700;900&family=Open+Sans:wght@300;400&family=Poppins:wght@400;500;700&display=swap" rel="stylesheet">
+
+    <!-- Box Icons -->
+    <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
+
+<!-- Custom CSS -->
+<link rel="stylesheet" href="css/style.css">
+    <title>Palm Sugar</title>
+  </head>
+  <body>
+  <header> 
+  <nav class="navbar navbar-expand-lg navbar-dark fixed-top py-3 bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"><span>Palm Sugar</span></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+      <div class="navbar-nav me-5 ">
+        <a class="nav-link active" href="#">Home</a>
+        <a class="nav-link me-3" href="#">Gallery</a>
+        <a class="nav-link me-3" href="#">About Us</a>
+        <a class="nav-link me-3" href="#">Team</a>
+        <a class="nav-link me-3" href="#">Contact Us</a>
+      </div>
+      <div class="icons-home mt-lg-0">
+      <a href="login.php"><i class='bx bx-user me-5'>Login</i></a>
+      </div>
+    </div>
+  </div>
+</nav>
+  </header>
+
+  <!-- home -->
+  <section class = "home" id="home">
+    <div class = "container">
+      <div class = "row">
+        <div class = "col-lg-6">
+          <h1 class="heading">Palm Sugar Purwokerto</h1>
+          <p class="subheading text-white">Dibuat dengan bahan olahan yang terbaik.</p>
+        </div>
+
+        <div class = "col-lg-6"></div>
+      </div>
+    </div>
+  </section>
+
+
+
+
+
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+  </body>
+</html>
